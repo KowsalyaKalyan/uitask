7bc47cb72950239cda9b38503ad4de2c2696e24a
KowsalyaKalyan <78580863+KowsalyaKalyan@users.noreply.github.com>
2022-06-09 23:54:03 +0530

    Initial commit

---
+++ lib/src/utils/constants.dart
@@ -0,0 +1,10 @@
+import 'package:uitask/allpackages.dart';
+
+class Constants {
+  static const yolet = Color(0xff893693);
+  static const primaryColor = Colors.white;
+  static const blackColor = Colors.black;
+  static const purple = Color(0xFFE1BEE7);
+  static const likesiconcolor = Color(0xffDADADA);
+  static const outerpurple = Color(0xFFD88AE6);
+}
